a63ecf61c89537dbc1c4873412ec8fdb985f7641
raypenper <41855946+raypenper@users.noreply.github.com>
2020-08-26 16:25:58 +0800

    Update gtrilti.h

--- gtrilti.h
+++ gtrilti.h
@@ -2,7 +2,7 @@
 Copyright (c) 2020, Deng fei. All rights reserved.
 QTI is available under two different licenses:
 GNU General Public License v3.0 (GPLv3)
-Commercial license (please contact at Deng fei(at)dengfei@cdut.cn)
+Commercial license (please contact at Deng Fei(at)dengfei@cdut.cn)
 /*************************************************/
 #ifndef GTRI_LTI_H
 #define GTRI_LTI_H
